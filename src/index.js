1e8feee3d4c496b85ba5e96c3edb54e637b6ceee
jollaman999 <admin@jollaman999.com>
2022-01-05 23:10:26 +0900

    Add createElement and Change jsx directive

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import { render } from './react.js';
+/* @jsx createElement */
+import { createElement, render } from './react.js';
 
-render(<div>Hello Tine React!</div>, document.getElementById('root'));
+function Title() {
+  return <h2>Hello Tiny React</h2>;
+}
+
+render(
+  <Title />,
+  <div>Hello Tine React!</div>,
+  document.getElementById('root')
+);
